15855817559a1912ef855a840515e8c211a77e0b
George Kibe <georgekibewambui@gmail.com>
2022-05-30 19:56:29 +0300

    quantity selector

--- src/screens/ProductScreen/index.tsx
+++ src/screens/ProductScreen/index.tsx
@@ -1,15 +1,20 @@
 import { View, Text } from 'react-native'
+import { useState } from 'react'
 import styles from './styles'
 import product from "../../data/product"
 import { Picker } from '@react-native-picker/picker'
+import QuantitySelector from '../../components/QuantitySelector'
 
 const ProductScreen = () => {
-    
+  const [selectedOption, setSelectedOption] = useState(product.options ? product.options[0] : null)
+  const [quantity, setQuantity] = useState(1)
   return (
     <View>
       <Text style={styles.title}>{product.title}</Text>
     
-      <Picker>
+      <Picker
+        selectedValue={selectedOption}
+        onValueChange={(itemValue) => setSelectedOption(itemValue)}>
         {product.options.map(option => (
             <Picker.Item label={option} value={option}/>
         ))}
@@ -20,6 +25,7 @@ const ProductScreen = () => {
       <Text style={styles.description}>
           {product.description}
       </Text>
+      <QuantitySelector quantity={quantity} setQuantity={setQuantity}/>
     </View>
   )
 }
