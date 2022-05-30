b5753029578bc7d23f13fb955e97553f14264140
George Kibe <georgekibewambui@gmail.com>
2022-05-30 19:10:41 +0300

    basic product page-static

--- src/screens/ProductScreen/index.tsx
+++ src/screens/ProductScreen/index.tsx
@@ -1,10 +1,25 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import styles from './styles'
+import product from "../../data/product"
+import { Picker } from '@react-native-picker/picker'
 
 const ProductScreen = () => {
+    
   return (
     <View>
-      <Text>ProductScreen</Text>
+      <Text style={styles.title}>{product.title}</Text>
+    
+      <Picker>
+        {product.options.map(option => (
+            <Picker.Item label={option} value={option}/>
+        ))}
+      </Picker>
+      <Text style={styles.price}>From Kshs. {product.price}
+        {product.oldPrice && (<Text style={styles.oldPrice}> Kshs. {product.oldPrice}</Text>)}
+      </Text>
+      <Text style={styles.description}>
+          {product.description}
+      </Text>
     </View>
   )
 }
--- src/screens/ProductScreen/styles.ts
+++ src/screens/ProductScreen/styles.ts
@@ -1,6 +1,24 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+    title:{
+
+    },
+    image:{
+
+    },
+    description:{
+
+    },
+    price:{
+        fontSize:18,
+        fontWeight:"bold"
+      },
+      oldPrice:{
+        fontSize:12,
+        fontWeight:"normal",
+        textDecorationLine:"line-through"
+      }
 
 })
 
